9dd2f7c18451d7e7cb6ea1b6e098ea37af794cf4
Dominik Fryč <fryc.dominik@gmail.com>
2024-01-01 15:24:26 +0100

    feat(demo): change demo to use typescript

---
+++ demo/src/main.ts
@@ -0,0 +1,37 @@
+// Import styles
+import 'diffix-test/themes/default.css';
+import 'diffix-test/themes/dark.css';
+import './styles.css';
+
+// Import all components
+import 'diffix-test';
+
+// Set theme as soon as possible to prevent flashing on page load
+const browserTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+const theme = localStorage.getItem('theme') || browserTheme;
+setTheme(theme);
+
+// Save theme to root element attribute and local storage
+function setTheme(theme: string): void {
+  document.documentElement.dataset.theme = theme;
+  localStorage.setItem('theme', theme);
+  setButtonIcon(theme);
+}
+
+// Show/hide svg icon inside toggle button based on theme
+function setButtonIcon(theme: string): void {
+  const toggleDark = document.getElementById('toggle-dark');
+  const toggleLight = document.getElementById('toggle-light');
+  if (toggleDark && toggleLight) {
+    toggleDark.style.display = theme === 'light' ? 'block' : 'none';
+    toggleLight.style.display = theme === 'dark' ? 'block' : 'none';
+  }
+}
+
+// Fade in body content after custom elements are defined
+customElements.whenDefined('dfx-button').then(() => document.body.classList.add('ready'));
+
+// Toggle theme on button click
+document.querySelector('#toggle-theme')?.addEventListener('dfx-click', () => {
+  setTheme(localStorage.getItem('theme') === 'light' ? 'dark' : 'light');
+});
